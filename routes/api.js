4c3a66698bf73a14a640c6d5f2826a9b8f8d1130
pinglinh <lnguyenmy1@gmail.com>
2018-01-24 14:02:42 +0000

    Added GET request

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const Ninja = require("../models/ninja");
+
 //get a list of ninjas from the db
 router.get("/ninjas", function(req, res, next) {
-  res.send({ type: "GET" });
+  Ninja.aggregate()
+    .near({
+      near: {
+        type: "point",
+        coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+      },
+      distanceField: "dist.calculated",
+      maxDistance: 100000,
+      spherical: true
+    })
+    .then(function(ninjas) {
+      res.send(ninjas);
+    })
+    .catch(next);
 });
 
 // add a new ninja to the db
