c79276a3fa44e625ff7a722239203d8ef8835107
pinglinh <lnguyenmy1@gmail.com>
2018-01-24 12:02:26 +0000

    Delete request added

--- routes/api.js
+++ routes/api.js
@@ -28,7 +28,9 @@ router.put("/ninjas/:id", function(req, res, next) {
 
 // delete a ninja in the db
 router.delete("/ninjas/:id", function(req, res, next) {
-  res.send({ type: "DELETE" });
+  Ninja.findByIdAndRemove({_id: req.params.id}).then(function(ninja) {
+    res.send(ninja);
+  }).catch(next);
 });
 
 module.exports = router;
